32c43836d7bbed0ee42d6af0b580df07bf2e8a76
Nate Rowan <nate_rowan1@baylor.edu>
2020-07-17 23:14:00 -0700

    create run value script

---
+++ scripts/05_create_run_metric.R
@@ -0,0 +1,9 @@
+############################
+### Name: Nate Rowan
+### Project: Sports Info Solutions 2020 Challenge
+### Date Created: 7/17/2020
+### Date Last Modified: 7/17/2020
+### Description: Creating a metric to evaluate the value of d-linemen on rushes
+############################
+library(tidyverse)
+
